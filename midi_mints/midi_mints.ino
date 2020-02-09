986dc54767da2d66a2f5e443a29d1cade2191b3d
Connor <rog.c@husky.neu.edu>
2020-02-09 15:50:51 -0500

    added code for 8x8 bicolor led matrix

--- midi_mints/midi_mints.ino
+++ midi_mints/midi_mints.ino
@@ -1,6 +1,9 @@
 #include <Wire.h>
 #include "Adafruit_MPR121.h"
 #include "MIDIUSB.h"
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
+#include "note_frames.h"
 
 // -------------- Cap-Touch Stuff ---------
 #ifndef _BV
@@ -17,19 +20,71 @@ uint16_t currtouched = 0;
 byte channel = 0;
 int octave = 0;
 
-void noteOn(byte pitch) {
+void noteOn(uint8_t pitch) {
   midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, 64};
   MidiUSB.sendMIDI(noteOn);
   MidiUSB.flush(); // Sends immediately
 }
 
-void noteOff(byte pitch) {
+void noteOff(uint8_t pitch) {
   midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, 64};
   MidiUSB.sendMIDI(noteOff);
   MidiUSB.flush(); // Sends immediately
 }
 // ----------------------------------------
 
+// ----------- LED Matrix Stuff -----------
+Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
+void dispNote(uint8_t pitch) {
+  matrix.clear();
+  switch(pitch % 12) {
+    case 0:
+      matrix.drawBitmap(0, 0, c_bmp, 8, 8, LED_RED);
+      break;
+    case 1:
+      matrix.drawBitmap(0, 0, df_bmp, 8, 8, LED_RED);
+      break;
+    case 2:
+      matrix.drawBitmap(0, 0, d_bmp, 8, 8, LED_RED);
+      break;
+    case 3:
+      matrix.drawBitmap(0, 0, ef_bmp, 8, 8, LED_RED);
+      break;
+    case 4:
+      matrix.drawBitmap(0, 0, e_bmp, 8, 8, LED_RED);
+      break;
+    case 5:
+      matrix.drawBitmap(0, 0, f_bmp, 8, 8, LED_RED);
+      break;
+    case 6:
+      matrix.drawBitmap(0, 0, gf_bmp, 8, 8, LED_RED);
+      break;
+    case 7:
+      matrix.drawBitmap(0, 0, g_bmp, 8, 8, LED_RED);
+      break;
+    case 8:
+      matrix.drawBitmap(0, 0, af_bmp, 8, 8, LED_RED);
+      break;
+    case 9:
+      matrix.drawBitmap(0, 0, a_bmp, 8, 8, LED_RED);
+      break;
+    case 10:
+      matrix.drawBitmap(0, 0, bf_bmp, 8, 8, LED_RED);
+      break;
+    case 11:
+      matrix.drawBitmap(0, 0, b_bmp, 8, 8, LED_RED);
+      break;
+    default:
+      matrix.clear();
+    matrix.setRotation(3);
+    matrix.writeDisplay();
+  }
+}
+void clearScreen() {
+  matrix.clear();
+  matrix.writeDisplay();
+}
+// ----------------------------------------
 void setup() {
 
   Serial.begin(115200);
@@ -42,6 +97,10 @@ void setup() {
     while (1);
   }
   // -------------------------------------  
+
+  // -------- 8x8 LED Matrix Setup -------
+  matrix.begin(0x70);
+  // -------------------------------------  
 }
 
 void loop() {
@@ -53,10 +112,12 @@ void loop() {
     // Detect press
     if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
       noteOn(i+48+12*octave); // NOTE ON
+      dispNote(i+48+12*octave);
     }
     // Detect release
     if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
       noteOff(i+48+12*octave); // NOTE OFF
+      clearScreen();
     }
   }
   // update buffer
